0ab1d069fc36bce61eb853c3435e5a3887b3b131
LZMMMM <liuzenming@gmail.com>
2026-05-17 13:08:02 +0800

    1125最终更改AC版

--- 1125.cpp
+++ 1125.cpp
@@ -102,57 +102,11 @@ public:
 int Point::counter = 0;
 int Point::sum = 0;
 int Point::pos = 0;
-void ShowPoint(Point p)
-{
-    cout<<std::setprecision(16)<<"Point : ("<<p.x()<<", "<<p.y()<<")"<<endl;
-}
- 
-void ShowPoint(double x, double y)
-{
-    Point p(x, y);
-    cout<<std::setprecision(16)<<"Point : ("<<p.x()<<", "<<p.y()<<")"<<endl;
-}
- 
-void ShowPoint(Point &p, double x, double y)
-{
-    cout<<std::setprecision(16)<<"Point : ("<<p.x(x)<<", "<<p.x(y)<<")"<<endl;
-}
- 
-int main()
-{
-    int l(0);
-    char c;
-    double a, b;
-    Point pt[60];
-    while(std::cin>>a>>c>>b)
-    {
-        if(a == b)
-            ShowPoint(pt[l].setPoint(a, b));
-        if(a > b)
-            ShowPoint(a, b);
-        if(a < b)
-            ShowPoint(pt[l], a, b);
-        l++;
-    }
-    Point p(a), q(b);
-    ShowPoint(q);
-    double x(0), y(0);
-    for(int i = 0; i < l; i++)
-        x += pt[i].getX(), y -= pt[i].getY();
-    ShowPoint(pt[l].setX(x), pt[l].setY(y));
-    cout<<"==========gorgeous separator=========="<<endl;
-    for(int i = 0; i <= l; i++)
-        pt[i].show();
-    q.setPoint(q.x() - p.x() + a, q.y() - p.y() + b).show();
-    q.show();
-    cout<<"==========gorgeous separator=========="<<endl;
-    p.showSumOfPoint();
-}
 
 
 
-//Appendcode:====================================================
 
+// ==================Append Code==================
 void ShowPoint(Point p)
 {
     cout<<std::setprecision(16)<<"Point : ("<<p.x()<<", "<<p.y()<<")"<<endl;
